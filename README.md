4310825f1b139aa4d01edcc1e4cbeff80c18b4fe
ArakiTakaki <araki_takaki@yahoo.co.jp>
2018-09-30 21:50:22 +0900

    mouse tracking

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # websoket-sample
+
+## データの送受信に関して
+
+| メソッド | 効果               |
+| :------- | :----------------- |
+| on       | データの受信を行う |
+| emit     | データの送信を行う |
+
+## サーバを介したやりとり
+
+サーバが仲介役となり、データの送受信を行う。
+
+### サーバ側
+
+`/app.js`から重要な部分の抜粋
+
+```javascript:app.js
+
+// serverには `http.createServer` の帰り値が入る
+// socket.ioのリスナを登録
+var io = socketio.listen(server);
+
+// io.sockets.on('event名', function );
+// Event名は任意で記述できるがわかりやすい方が良いよね。
+io.sockets.on('connection', function(s){
+  socket.on('client_to_server, function(data){
+    io.sockets.emit('servet_to_client', {value: data.value})
+  })
+})
+
+```
+
+#### connection
+
+クライアントが接続を確率する際に発火する、このなかの処理に関しては接続している限り有効となる。
+
+#### client_to_server
+
+クライアントがサーバに対してアクションを起こした時点で発火
+
+#### server_to_client
+
+サーバがクライアントにデータを送信した時点で発火
+
+onでリスナーの作成を行い、emitで発火させると覚える
--- app.js
+++ app.js
@@ -1,15 +1,41 @@
+// socket http fs
 var http = require('http');
 var socketio = require('socket.io');
+// ファイルシステム
 var fs = require('fs');
+
+// httpサーバのcreate
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type' : 'text/html'});
+
+    //ディレクトリ＋index.htmlを読み込む
     res.end(fs.readFileSync(__dirname + '/index.html', 'utf-8'));
 }).listen(3000);  // ポート競合の場合は値を変更
 
+// websocketのリスナーをserverに追加する。
 var io = socketio.listen(server);
 
+io.set('heartbeat interval', 5000);
+io.set('heartbeat timeout', 15000);
+
+//接続時のEvent Handle
 io.sockets.on('connection', function(socket) {
-    socket.on('client_to_server', function(data) {
-        io.sockets.emit('server_to_client', {value : data.value});
+  console.log('connectを確率しました');
+  //clientからデータを受信する。
+  socket.on('client_to_server', function(data) {
+    //clientからデータを送信する。
+    io.sockets.emit('server_to_client', {
+      value : data.value,
+      typeMsg: data.typeMsg,
+      grid: data.grid,
     });
+  });
 });
+
+io.sockets.on('disconnect', function(){
+  console.log("dissconnect");
+});
+
+console.log('test');
+//emit データの送信 response
+//on データの受信 request
